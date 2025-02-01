1be96e59636d4891f498062fad404523772d1ec0
Juan Sebastian Gonzalez Camacho <sebasgonzac27@gmail.com>
2025-02-01 17:24:38 -0500

    feat: :sparkles: new features

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export const CONFIG = {
   API_URL: import.meta.env.VITE_API_URL,
+  APP_NAME: import.meta.env.VITE_APP_NAME,
 }
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -2,29 +2,33 @@ import { AuthLayout } from '@/layouts'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, LoginForm } from '@/components'
 import { Link } from 'react-router-dom'
 import { PublicRoutes } from '@/routes'
+import Logo from '@/assets/img/logo.webp'
 
 export default function LoginPage() {
   return (
     <AuthLayout>
-      <Card className='w-full max-w-2xl'>
-        <CardHeader>
-          <CardTitle>Iniciar Sesión</CardTitle>
-          <CardDescription>Manténte actualizado sobre los eventos de tu club.</CardDescription>
-        </CardHeader>
-        <CardContent className='space-y-2'>
-          <LoginForm />
-        </CardContent>
-        <CardFooter className='flex flex-col'>
-          <p className='mx-auto text-center'>
-            ¿No tienes una cuenta?
-            <Link
-              className='ml-2 cursor-pointer font-semibold text-primary hover:underline'
-              to={`/${PublicRoutes.REGISTER}`}>
-              Regístrate
-            </Link>
-          </p>
-        </CardFooter>
-      </Card>
+      <div className='flex flex-col items-center justify-center w-full h-full space-y-8'>
+        <img src={Logo} alt='Logo Club' className='w-[40vw] max-w-[250px]' />
+        <Card className='w-full max-w-2xl'>
+          <CardHeader>
+            <CardTitle>Iniciar Sesión</CardTitle>
+            <CardDescription>Manténte actualizado sobre la información de tu club.</CardDescription>
+          </CardHeader>
+          <CardContent className='space-y-2'>
+            <LoginForm />
+          </CardContent>
+          <CardFooter className='flex flex-col'>
+            <p className='mx-auto text-center'>
+              ¿No tienes una cuenta?
+              <Link
+                className='ml-2 cursor-pointer font-semibold text-primary hover:underline'
+                to={`/${PublicRoutes.REGISTER}`}>
+                Regístrate
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
     </AuthLayout>
   )
 }
